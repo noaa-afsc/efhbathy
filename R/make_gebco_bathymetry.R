7737803d9bcd5835dc72c7e8ac04b40d49c4ae1a
sean-rohan-NOAA <sean.rohan@noaa.gov>
2022-11-17 09:06:52 -0800

    Use new akgfmaps function to generate GEBCO bathymetry, slope, and aspect

--- R/make_gebco_bathymetry.R
+++ R/make_gebco_bathymetry.R
@@ -2,31 +2,38 @@ library(akgfmaps)
 library(cowplot)
 library(RODBC)
 
-dir.create(path = here::here("output", "EBS"))
-
 source(here::here("R", "get_connected.R"))
-
-# Load GEBCO netCTD file (subset of full 2022 GEBCO bathymetry- downloaded on October 26, 2022)
-gebco_2022 <- raster::raster(here::here("data", "gebco_2022_n75.0_s50.0_w-179.99_e-130.0.nc"), 
-                             varname = "elevation", # Elevation (up = positive)
-                             crs = "EPSG:4269") # WGS84
 
 # Load EFH bathymetry
 ebs_bathy_raster <- raster::raster(here::here("data", "EBS", "Bathy.grd"))
 
-# Raster of -1 and NA where -1 indicates locations with data in the EFH bathymety. Multiplying this by the GEBCO will (1) transform GEBCO elevation (up = positive) to same scale as EBS bathymetry (down = positive) and (2) mask the transformed GEBCO raster to locations with data without re-masking using the survey area shapefile
-ebs_bathy_locs <- round(ebs_bathy_raster/1e8) - 1
+# Project GEBCO to the extent and resolution of the EBS bathymetry raster in the CRS of the bathymetry raster (function in akgfmaps version >= 2.3.0)
+gebco_epsg3338 <- akgfmaps::reproject_gebco(x = here::here("data", "gebco_2022_n75.0_s50.0_w-179.99_e-130.0.nc"),
+                                            z_varname = "elevation", # Elevation (up = positive)
+                                            raster_template = ebs_bathy_raster,
+                                            z_direction = -1, # Elevation (up = positive)
+                                            return_slope_aspect = TRUE)
 
-# Project GEBCO to the extent and resolution of the EBS bathymetry raster in the CRS of the bathymetry raster
-gebco_epsg3338 <- raster::projectRaster(from = gebco_2022, to = ebs_bathy_raster, method = "bilinear")
+# Raster of 1 and NA where 1 indicates locations with data in the EFH bathymety. Multiplying this by the GEBCO will effectively mask the transformed GEBCO raster to locations with data without re-masking using the survey area shapefile
+ebs_bathy_locs <- round(ebs_bathy_raster/1e8) + 1
 
 # Multiply by binary raster to only include data at the same locations as the EBS bathymetry raster
+layer_names <- names(gebco_epsg3338)
 gebco_epsg3338 <- gebco_epsg3338*ebs_bathy_locs
+names(gebco_epsg3338) <- layer_names
 
-names(gebco_epsg3338) <- "seafloor_depth"
+# Write bathymetry, slope, and aspect rasters to .grd files
+writeRaster(x = gebco_epsg3338$seafloor_depth, 
+            filename = here::here("output", "EBS", "gebco_bathymetry.grd"),
+            overwrite = TRUE)
 
-# Write bathymetry raster to .grd file
-writeRaster(x = gebco_epsg3338, filename = here::here("output", "EBS", "gebco_bathymetry.grd"))
+writeRaster(x = gebco_epsg3338$slope, 
+            filename = here::here("output", "EBS", "gebco_slope.grd"),
+            overwrite = TRUE)
+
+writeRaster(x = gebco_epsg3338$aspect, 
+            filename = here::here("output", "EBS", "gebco_aspect.grd"),
+            overwrite = TRUE)
 
 # Compare EBS EFH 2022 bathymetry raster to GEBCO
 ebs_bathy_raster_df <- ebs_bathy_raster |>
@@ -44,16 +51,39 @@ print(
                           mapping = aes(x = x, y = y, z = seafloor_depth),
                           breaks = c(seq(0,200,10), Inf)) +
       scale_fill_viridis_d(name = "Depth (m)") +
-      ggtitle("ARDEM v2") +
+      ggtitle("GEBCO bathymetry") +
       theme(legend.position = "bottom"),
     ggplot() +
       geom_contour_filled(data = ebs_bathy_raster_df,
                           mapping = aes(x = x, y = y, z = ebs_bath5hac),
                           breaks = c(seq(0,200,10), Inf)) +
       scale_fill_viridis_d(name = "Depth (m)") +
-      ggtitle("EBS_bathy") +
+      ggtitle("2022 EFH Bathymetry") +
       theme(legend.position = "bottom")
   )
+)
+dev.off()
+
+png(here::here("plots", "EBS_GEBCO_slope_aspect.png"), width = 18, height = 10, units = "in", res = 300)
+print(
+  cowplot::plot_grid(
+    ggplot() +
+      geom_tile(data = gebco_df,
+                mapping = aes(x = x, 
+                              y = y, 
+                              fill = cut(slope, c(0, 0.1, 0.2, seq(0.4, 2, 0.4), 10, 40), right = FALSE))) +
+      scale_fill_viridis_d(name = expression(Slope~(degree)), drop = FALSE, option = "C") +
+      ggtitle("GEBCO Slope") +
+      theme(legend.position = "bottom"),
+    ggplot() +
+      geom_tile(data = gebco_df,
+                mapping = aes(x = x, 
+                              y = y, 
+                              fill = cut(aspect, breaks = seq(0,360,45)))) +
+      scale_fill_viridis_d(name = expression(Aspect~(degree)), drop = FALSE, option = "A") +
+      ggtitle("GEBCO Aspect") +
+      theme(legend.position = "bottom")
+    )
 )
 dev.off()
 
