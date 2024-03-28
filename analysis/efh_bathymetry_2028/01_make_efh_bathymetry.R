1a5f9324c80f5a921dbd2c32120cf3b7878b5d8b
sean-rohan-NOAA <sean.rohan@noaa.gov>
2024-03-28 16:09:22 -0700

    Reorganize

--- analysis/efh_bathymetry_2028/01_make_efh_bathymetry.R
+++ analysis/efh_bathymetry_2028/01_make_efh_bathymetry.R
@@ -2,40 +2,47 @@ library(akgfmaps)
 library(reticulate)
 
 
-dir.create(here::here("data", "efh_bathy"), recursive = TRUE)
-dir.create(here::here("output", "efh_bathy"), recursive = TRUE)
+dir.create(here::here("analysis", "efh_bathymetry_2028", "data", "efh_bathy"), recursive = TRUE)
+dir.create(here::here("analysis", "efh_bathymetry_2028", "output", "efh_bathy"), recursive = TRUE)
 
 # Transform GEBCO Z coordinates to match Alaska bathymetry (negative is up)
-gebco <- terra::rast(here::here("data", "gebco_2023_n67.0_s54.0_w-175.0_e-154.0.tif"))
+gebco <- terra::rast(here::here("analysis", "efh_bathymetry_2028", "data", "gebco_2023_n67.0_s54.0_w-175.0_e-154.0.tif"))
 gebco <- gebco * -1
 
 map_layers <- akgfmaps::get_base_layers(select.region = "sebs", set.crs = crs(gebco, proj = TRUE))
 
 gebco <- terra::mask(gebco, map_layers$akland[2,], inverse = TRUE)
 
-terra::writeRaster(gebco, filename = here::here("data", "data", "gebco_2023_inverse.tif"))
+terra::writeRaster(gebco, filename = here::here("analysis", "efh_bathymetry_2028", "data", "gebco_2023_inverse.tif"))
 
 
 # ArcPy
 # Python 3.9 with ArcGIS pro version 3.1
 
+reticulate::use_python("C:/Program Files/Python39/python.exe")
+reticulate::source_python(file = here::here("analysis", "efh_bathymetry_2028", "make_efh_bathy.py"))
+
 
 ## Trim blank areas around the edge and rename the depth band
 
-efh_bathy_trim <- terra::rast(here::here("data", "efh_bathy", "efh_bathy_100.tif")) |>
+efh_bathy_trim <- terra::rast(here::here("analysis", "efh_bathymetry_2028", "data", "efh_raw_100m.tif")) |>
   terra::trim()
 
 names(efh_bathy_trim) <- "depth"
 
-terra::writeRaster(efh_bathy_trim, here::here("output", "efh_bathy", "efh_bathy_100m.tif"), overwrite = TRUE)
+terra::writeRaster(efh_bathy_trim, here::here("analysis", "efh_bathymetry_2028", "output", "efh_bathy", "efh_bathy_100m.tif"), overwrite = TRUE)
 
 
 # Resample the 100 m resolution grid to 1 km
-efh_100m <- terra::rast(here::here("data", "efh_bathy", "efh_bathy_100m.tif"))
+efh_100m <- terra::rast(here::here("analysis", "efh_bathymetry_2028", "output", "efh_bathy", "efh_bathy_100m.tif"))
 
 efh_1km <- terra::aggregate(efh_100m, fact = 10, fun = "mean")
 
-terra::writeRaster(efh_1km, here::here("output", "efh_bathy", "efh_bathy_1km.tif"), overwrite = TRUE)
+terra::writeRaster(efh_1km, here::here("analysis", "efh_bathymetry_2028", "output", "efh_bathy", "efh_bathy_1km.tif"), overwrite = TRUE)
+
+
+
+
 
 
 
--- analysis/efh_bathymetry_2028/02_derive_terrain_variables.R
+++ analysis/efh_bathymetry_2028/02_derive_terrain_variables.R
@@ -5,7 +5,7 @@
 library(akgfmaps)
 library(MultiscaleDTM) # last run: version 0.8.3
 
-dir.create(here::here("output"))
+dir.create(here::here("analysis", "efh_bathymetry_2028", "output", "terrain"))
 
 # 2023 Review: Queen case (3x3 neighborhood) for deriving seafloor terrain
 neighbors <- 8
@@ -13,7 +13,7 @@ bpi_w = c(3, 64) # 2023 Review: AI and GOA Radius 3 x 65; EBS radius 3 x 64
 
 
 # Calculate terrain variables for 100-m resolution
-bathy_100m <- terra::rast(here::here("output", "efh_bathy", "efh_bathy_100m.tif"))
+bathy_100m <- terra::rast(here::here("analysis", "efh_bathymetry_2028", "output", "efh_bathy", "efh_bathy_100m.tif"))
 
 # SLOPE using terra ----
 start1 <- Sys.time()
@@ -56,28 +56,28 @@ difftime(start2, end2)
 # Write 100-m rasters to GeoTIFF files ----
 
 writeRaster(bpi_dtm_100m, 
-            here::here("output", "efh_bpi_100m.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_bpi_100m.tif"), 
             overwrite = TRUE)
 writeRaster(curvature_slope_dtm_100m$meanc, 
-            here::here("output", "efh_dtm_meancurv_100m.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_dtm_meancurv_100m.tif"), 
             overwrite = TRUE)
 writeRaster(curvature_slope_dtm_100m$profc, 
-            here::here("output", "efh_dtm_profcurv_100m.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_dtm_profcurv_100m.tif"), 
             overwrite = TRUE)
 writeRaster(curvature_slope_dtm_100m$qslope, 
-            here::here("output", "efh_dtm_qslope_100m.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_dtm_qslope_100m.tif"), 
             overwrite = TRUE)
 writeRaster(slope_aspect_dtm_100m$northness, 
-            here::here("output", "efh_dtm_northness_100m.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_dtm_northness_100m.tif"), 
             overwrite = TRUE)
 writeRaster(slope_aspect_dtm_100m$eastness, 
-            here::here("output", "efh_dtm_eastness_100m.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_dtm_eastness_100m.tif"), 
             overwrite = TRUE)
 writeRaster(slope_aspect_dtm_100m$slope, 
-            here::here("output", "efh_dtm_slope_100m.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_dtm_slope_100m.tif"), 
             overwrite = TRUE)
 writeRaster(slope_t_100m,
-            here::here("output", "efh_t_slope_100m.tif"),
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_t_slope_100m.tif"),
             overwrite = TRUE)
 
 
@@ -94,26 +94,26 @@ bpi_dtm_1km <- terra::aggregate(bpi_dtm, fact = 10, fun = "mean")
 # Write 1-km rasters to GeoTIFF files ----
 
 writeRaster(bpi_dtm_1km, 
-            here::here("output", "efh_bpi_1km.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_bpi_1km.tif"), 
             overwrite = TRUE)
 writeRaster(curvature_slope_dtm_1km$meanc, 
-            here::here("output", "efh_dtm_meancurv_1km.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_dtm_meancurv_1km.tif"), 
             overwrite = TRUE)
 writeRaster(curvature_slope_dtm_1km$profc, 
-            here::here("output", "efh_dtm_profcurv_1km.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_dtm_profcurv_1km.tif"), 
             overwrite = TRUE)
 writeRaster(curvature_slope_dtm_1km$qslope, 
-            here::here("output", "efh_dtm_qslope_1km.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_dtm_qslope_1km.tif"), 
             overwrite = TRUE)
 writeRaster(slope_aspect_dtm_1km$northness, 
-            here::here("output", "efh_dtm_northness_1km.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_dtm_northness_1km.tif"), 
             overwrite = TRUE)
 writeRaster(slope_aspect_dtm_1km$eastness, 
-            here::here("output", "efh_dtm_eastness_1km.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_dtm_eastness_1km.tif"), 
             overwrite = TRUE)
 writeRaster(slope_aspect_dtm_1km$slope, 
-            here::here("output", "efh_dtm_slope_1km.tif"), 
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_dtm_slope_1km.tif"), 
             overwrite = TRUE)
 writeRaster(slope_t_1km,
-            here::here("output", "efh_t_slope_1km.tif"),
+            here::here("analysis", "efh_bathymetry_2028", "output", "terrain", "efh_t_slope_1km.tif"),
             overwrite = TRUE)
