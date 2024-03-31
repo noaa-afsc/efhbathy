caabc06309da4227e0818f9233a39534f13df08b
sean-rohan-NOAA <sean.rohan@noaa.gov>
2024-03-30 20:56:01 -0700

    Add RKC

--- analysis/efh_bathymetry_2028/assets/bpi_inner_outer.R
+++ analysis/efh_bathymetry_2028/assets/bpi_inner_outer.R
@@ -2,16 +2,10 @@ library(akgfmaps)
 library(MultiscaleDTM)
 source(here::here("R", "bbox_to_polygon.R")) # Function to be included in an upcoming akgfmaps release
 
-zone = "Nazan Bay"
+# zone = "Nazan Bay"
+zone = "RKC Savings Area"
 
 locations <- read.csv(file = here::here("analysis", "efh_bathymetry_2028", "data", "terrain_example_zones.csv"))
-
-# locations <- data.frame(x = -173.9335093, 
-#                         y = 52.1756401, 
-#                         label = "Nazan Bay", 
-#                         region = "ai", 
-#                         focal_buffer = 15000, 
-#                         extended_buffer = 1e5)
 
 set_loc <- dplyr::filter(locations, 
                          label == zone)
@@ -21,7 +15,7 @@ map_layers <- akgfmaps::get_base_layers(select.region = set_loc$region, set.crs 
 # Load 100 m resolution raster and tow paths ----
 efh_100m <- terra::rast(here::here("analysis", "efh_bathymetry_2028", "output", "efh_bathy", "efh_bathy_100m.tif"))
 
-region_towpath <- sf::st_read(here::here("analysis", "efh_bathymetry_2028", "data", "bts_tows", paste0(locations$region, "_towpath.shp"))) |>
+region_towpath <- sf::st_read(here::here("analysis", "efh_bathymetry_2028", "data", "bts_tows", paste0(set_loc$region, "_towpath.shp"))) |>
   sf::st_transform(crs = map_layers$crs$input) |>
   dplyr::filter(CRUISE > 199000) |>
   dplyr::mutate(PERFORMANCE_LABEL = factor(sign(PERFORM), levels = c(-1, 0, 1), labels = c("Bad", "Good", "Acceptable")))
@@ -37,13 +31,13 @@ focal_zone <- set_loc |>
   sf::st_as_sf(coords = c("x", "y"), crs = "WGS84") |>
   sf::st_transform(crs = map_layers$crs$input)
 
-plot_bbox <- sf::st_buffer(focal_zone, dist = 15000) |>
+plot_bbox <- sf::st_buffer(focal_zone, dist = set_loc$focal_buffer) |>
   sf::st_bbox()
 
-focal_bbox <- sf::st_buffer(focal_zone, dist = 15000) |>
+focal_bbox <- sf::st_buffer(focal_zone, dist = set_loc$focal_buffer) |>
   bbox_to_polygon()
 
-expanded_bbox <- sf::st_buffer(focal_zone, dist = 100000) |>
+expanded_bbox <- sf::st_buffer(focal_zone, dist = set_loc$extended_buffer) |>
   bbox_to_polygon()
 
 expanded_bathy <- terra::crop(efh_100m, 
@@ -52,19 +46,23 @@ expanded_bathy <- terra::crop(efh_100m,
 focal_bathy <- terra::mask(expanded_bathy, 
                            focal_bbox)
 
-# Calculate BPI following Lundblad et al. (2006) ----
+plot(focal_bathy)
+
+# Calculate BPI following the 2023 method from Lundblad et al. (2006) ----
 
 bpi_focal_2023_method <- MultiscaleDTM::BPI(r = focal_bathy, 
                                             w = c(3, 64), 
                                             unit = "cell", 
-                                            na.rm = TRUE)
+                                            na.rm = TRUE) |>
+  terra::crop(focal_bathy)
 
 bpi_focal_2023_method <- round(bpi_focal_2023_method + 0.5)
 
 bpi_expanded_2023_method <- MultiscaleDTM::BPI(r = expanded_bathy, 
                                                w = c(3, 64), 
                                                unit = "cell", 
-                                               na.rm = TRUE)
+                                               na.rm = TRUE) |>
+  terra::crop(focal_bathy)
 
 bpi_expanded_2023_method <- round(bpi_expanded_2023_method + 0.5)
 
@@ -74,7 +72,7 @@ plot_focal_bathy <- ggplot() +
   geom_sf(data = region_towpath,
           mapping = aes(color = PERFORMANCE_LABEL)) +
   scale_color_manual(name = "Performance", values = performance_color) +
-  scale_fill_distiller(name = "Depth (m)", na.value = "grey70", direction = 1) +
+  scale_fill_distiller(name = "Depth (m)", na.value = "#BED2FF", direction = 1) +
   coord_sf(xlim = c(plot_bbox['xmin'], plot_bbox['xmax']) + c(-3000, 3000),
            ylim = c(plot_bbox['ymin'], plot_bbox['ymax']) + c(-3000, 3000),
            expand = 0) +
@@ -88,7 +86,7 @@ plot_focal_bpi_2023_method <- ggplot() +
           mapping = aes(color = PERFORMANCE_LABEL)) +
   scale_color_manual(name = "Performance", values = performance_color) +
   scale_fill_distiller(name = "Focal BPI", 
-                       na.value = "grey70", 
+                       na.value = "#BED2FF", 
                        direction = 1, 
                        # limits = bpi_range, 
                        palette = "BrBG") +
@@ -100,16 +98,13 @@ plot_focal_bpi_2023_method <- ggplot() +
 
 
 plot_expanded_bpi_2023_method <- ggplot() +
-  geom_stars(data = stars::st_as_stars(
-    terra::crop(bpi_expanded_2023_method, focal_bbox)
-    )
-    ) +
+  geom_stars(data = stars::st_as_stars(bpi_expanded_2023_method)) +
   geom_sf(data = map_layers$akland, fill = "grey30", color = NA) +
   geom_sf(data = region_towpath,
           mapping = aes(color = PERFORMANCE_LABEL)) +
   scale_color_manual(name = "Performance", values = performance_color) +
   scale_fill_distiller(name = "Extended BPI", 
-                       na.value = "grey70", 
+                       na.value = "#BED2FF", 
                        direction = 1, 
                        # limits = bpi_range, 
                        palette = "BrBG") +
@@ -125,7 +120,10 @@ plot_expanded_bpi_2023_diff <- ggplot() +
   geom_sf(data = region_towpath,
           mapping = aes(color = PERFORMANCE_LABEL)) +
   scale_color_manual(name = "Performance", values = performance_color) +
-  scale_fill_distiller(name = "Extended-Focal", na.value = "grey70", direction = 1, palette = "PuOr") +
+  scale_fill_distiller(name = "Extended-Focal", 
+                       na.value = "#BED2FF", 
+                       direction = 1, 
+                       palette = "PuOr") +
   coord_sf(xlim = c(plot_bbox['xmin'], plot_bbox['xmax']) + c(-3000, 3000),
            ylim = c(plot_bbox['ymin'], plot_bbox['ymax']) + c(-3000, 3000),
            expand = 0) +
@@ -142,22 +140,28 @@ cowplot::plot_grid(plot_focal_bathy,
 # Keep in mind that the typical tow paths is ~1.5 km
 
 bpi_15_64 <- MultiscaleDTM::BPI(r = expanded_bathy, 
-                                         w = c(15, 64), 
-                                         unit = "cell", 
-                                         na.rm = TRUE)
+                                w = c(15, 64), 
+                                unit = "cell", 
+                                na.rm = TRUE) |>
+  terra::crop(focal_bbox)
 
 bpi_15_30 <- MultiscaleDTM::BPI(r = expanded_bathy, 
-                                         w = c(15, 30), 
-                                         unit = "cell", 
-                                         na.rm = TRUE)
-
-bpi_3_10 <- MultiscaleDTM::BPI(r = expanded_bathy, 
-                                        w = c(3, 10), unit = "cell", na.rm = TRUE)
+                                w = c(15, 30), 
+                                unit = "cell", 
+                                na.rm = TRUE) |>
+  terra::crop(focal_bbox)
 
 bpi_3_15 <- MultiscaleDTM::BPI(r = expanded_bathy, 
-                                        w = c(3, 15), 
-                                        unit = "cell", 
-                                        na.rm = TRUE)
+                               w = c(3, 15), 
+                               unit = "cell", 
+                               na.rm = TRUE) |>
+  terra::crop(focal_bbox)
+
+bpi_3_10 <- MultiscaleDTM::BPI(r = expanded_bathy, 
+                               w = c(3, 10), 
+                               unit = "cell", 
+                               na.rm = TRUE) |>
+  terra::crop(focal_bbox)
 
 plot_bpi_15_64 <- ggplot() +
   geom_stars(data = stars::st_as_stars(bpi_15_64)) +
@@ -166,7 +170,7 @@ plot_bpi_15_64 <- ggplot() +
           mapping = aes(color = PERFORMANCE_LABEL)) +
   scale_color_manual(name = "Performance", values = performance_color) +
   scale_fill_distiller(name = "BPI (15, 64)", 
-                       na.value = "grey70", 
+                       na.value = "#BED2FF", 
                        direction = 1, 
                        # limits = bpi_range, 
                        palette = "BrBG") +
@@ -183,7 +187,7 @@ plot_bpi_15_30 <- ggplot() +
           mapping = aes(color = PERFORMANCE_LABEL)) +
   scale_color_manual(name = "Performance", values = performance_color) +
   scale_fill_distiller(name = "BPI (15, 30)", 
-                       na.value = "grey70", 
+                       na.value = "#BED2FF", 
                        direction = 1, 
                        # limits = bpi_range, 
                        palette = "BrBG") +
@@ -200,7 +204,7 @@ plot_bpi_3_15 <- ggplot() +
           mapping = aes(color = PERFORMANCE_LABEL)) +
   scale_color_manual(name = "Performance", values = performance_color) +
   scale_fill_distiller(name = "BPI (3, 15)", 
-                       na.value = "grey70", 
+                       na.value = "#BED2FF", 
                        direction = 1, 
                        # limits = bpi_range, 
                        palette = "BrBG") +
@@ -220,7 +224,7 @@ plot_bpi_3_10 <- ggplot() +
           linewidth = rel(1.1)) +
   scale_color_manual(name = "Performance", values = performance_color) +
   scale_fill_distiller(name = "BPI (3, 10)", 
-                       na.value = "grey70", 
+                       na.value = "#BED2FF", 
                        direction = 1, 
                        # limits = bpi_range, 
                        palette = "BrBG") +
@@ -229,7 +233,6 @@ plot_bpi_3_10 <- ggplot() +
            expand = 0) +
   theme_bw() +
   theme(axis.title = element_blank())
-
 
 cowplot::plot_grid(plot_bpi_15_64, 
                    plot_bpi_15_30, 
@@ -262,7 +265,7 @@ plot_bpi_15_64_1km <- ggplot() +
           mapping = aes(color = PERFORMANCE_LABEL)) +
   scale_color_manual(name = "Performance", values = performance_color) +
   scale_fill_distiller(name = "BPI (15, 64)", 
-                       na.value = "grey70", 
+                       na.value = "#BED2FF", 
                        direction = 1, 
                        # limits = bpi_range, 
                        palette = "BrBG") +
@@ -279,7 +282,7 @@ plot_bpi_15_30_1km <- ggplot() +
           mapping = aes(color = PERFORMANCE_LABEL)) +
   scale_color_manual(name = "Performance", values = performance_color) +
   scale_fill_distiller(name = "BPI (15, 30)", 
-                       na.value = "grey70", 
+                       na.value = "#BED2FF", 
                        direction = 1, 
                        # limits = bpi_range, 
                        palette = "BrBG") +
@@ -296,7 +299,7 @@ plot_bpi_3_10_1km <- ggplot() +
           mapping = aes(color = PERFORMANCE_LABEL)) +
   scale_color_manual(name = "Performance", values = performance_color) +
   scale_fill_distiller(name = "BPI (3, 10)", 
-                       na.value = "grey70", 
+                       na.value = "#BED2FF", 
                        direction = 1, 
                        # limits = bpi_range, 
                        palette = "BrBG") +
@@ -313,7 +316,7 @@ plot_bpi_3_15_1km <- ggplot() +
           mapping = aes(color = PERFORMANCE_LABEL)) +
   scale_color_manual(name = "Performance", values = performance_color) +
   scale_fill_distiller(name = "BPI (3, 15)", 
-                       na.value = "grey70", 
+                       na.value = "#BED2FF", 
                        direction = 1, 
                        # limits = bpi_range, 
                        palette = "BrBG") +
